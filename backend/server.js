d4a1f5019f977868e8810d90b8c9cf5bfc7eb32e
sam88an@gmail.com <sam88an@gmail.com>
2021-02-08 22:55:24 +0700

    23h 08/02/2021

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,25 @@
 const express = require("express");
 const data = require("./data.js");
+
 const app = express();
+
+app.get("/api/products/:id", (req, res) => {
+  const product = data.products.find((x) => x._id === req.params.id);
+  if (product) {
+    res.send(product);
+  } else {
+    res.status(404).send({ message: "Product Not Found" });
+  }
+});
+
 app.get("/api/products", (req, res) => {
   res.send(data.products);
 });
-app.get((req, res) => {
-  res.send("Server is ready...");
+
+app.get("/", (req, res) => {
+  res.send("Server is ready");
 });
-app.listen(5000, () => {
-  console.log("Server run in port 5000...");
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+  console.log(`Serve at http://localhost:${port}`);
 });
